7a599d47a8f844719a35371001dd879079f959f5
AlexW0lf1 <115431222+AlexW0lf1@users.noreply.github.com>
2024-05-30 09:25:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="pandoc-docx-pagebreak-alex",  # Required
+    name="pandoc-docx-pagebreak",  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
